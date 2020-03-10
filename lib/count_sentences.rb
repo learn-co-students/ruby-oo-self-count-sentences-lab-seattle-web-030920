dd9151bb8a2756f16a58d7c4cdd245466a59b5fc
John Souza <souzajohnlee@gmail.com>
2020-03-10 23:10:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    total = 0 
+    self.split(" ").each {|word| word.sentence? || word.question? || word.exclamation? ? total += 1 : nil}
+    return total
   end
+  
 end
