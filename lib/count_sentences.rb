e5f3dbd661fa18ba75ba4010b9d0b439d6319215
jeff_simon <jeffsbillinfo@gmail.com>
2020-03-11 16:14:20 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,7 @@ class String
   def count_sentences
     array = self.split(" ")
     #array.select{|x| x=="." || x=="?"}.length
-    array.select{|x| x.end_with?(".")||x.end_with?("?")||x.end_with?("!")}.length
-  
+    #array.select{|x| x.end_with?(".")||x.end_with?("?")||x.end_with?("!")}.length  
+    array.select{|x| x.exclamation?||x.question?||x.sentence?}.length  
   end
 end
